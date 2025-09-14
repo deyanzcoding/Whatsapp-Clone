c420eb17663dd75546484f5abbd1e99f974d6c77
deyanzcoding <deyanzcoding@gmail.com>
2025-09-14 11:56:40 +0500

    making ui_helper class

--- lib/Widgets/ui_helper.dart
+++ lib/Widgets/ui_helper.dart
@@ -1,12 +1,36 @@
-
-
+import 'package:flutter/material.dart';
 import 'dart:ui';
 
 class UiHelper {
- static CustomButton({
-   required VoidCallback callback,
-   required String buttonname,
- }) {
+  static CustomButton({
+    required VoidCallback callback,
+    required String buttonname,
+  }) {
+    return SizedBox(
+      height: 35,
+      width: 300,
+      child: ElevatedButton(
+        onPressed: () {
+          callback();
+        },
 
- }
-}
+        child: Text(
+          buttonname,
+          style: TextStyle(fontSize: 14, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  static CustomText({
+    required String text,
+    required double height,
+    Color? color,
+    FontWeight? fontweight,
+  }) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: height, color: color ?? Color(0xff5e5e5e), fontweight),
+    );
+  }
+} //customeButton
