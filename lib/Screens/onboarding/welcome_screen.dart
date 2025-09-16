8ae91b21a8bed7f731721a2fd8271c3d7fa6cfce
deyanzcoding <deyanzcoding@gmail.com>
2025-09-16 21:54:06 +0500

    Update welcome_screen.dart

--- lib/Screens/onboarding/welcome_screen.dart
+++ lib/Screens/onboarding/welcome_screen.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/Screens/onboarding/login_screen.dart';
 import 'package:whatsapp_clone/Widgets/ui_helper.dart';
 
-class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
+class WelcomeScreen extends StatefulWidget {
+  WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  String selectedLang = "English";
+
+  List<String> languages = ["Urdu", "Pashto", "Chinese", "Japanese", "Korean"];
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +21,7 @@ class WelcomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset("assets/images/welcome_whatsapp.png", scale: 3,),
+            Image.asset("assets/images/welcome_whatsapp.png", scale: 3),
             SizedBox(height: 40),
             UiHelper.CustomText(
               text: "Welcome to WhatsApp",
@@ -34,14 +43,22 @@ class WelcomeScreen extends StatelessWidget {
                     TextSpan(text: "Read our "),
                     TextSpan(
                       text: "Privacy Policy",
-                      style: TextStyle(color: Color(0xff0C42CC), fontSize: 15, fontWeight: FontWeight.w600),
+                      style: TextStyle(
+                        color: Color(0xff0C42CC),
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                     TextSpan(
                       text: ". Tap \"Agree and continue\" to accept the ",
                     ),
                     TextSpan(
                       text: "Terms of Service.",
-                      style: TextStyle(color: Color(0xff0C42CC), fontSize: 15, fontWeight: FontWeight.w600),
+                      style: TextStyle(
+                        color: Color(0xff0C42CC),
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ],
                 ),
@@ -49,34 +66,30 @@ class WelcomeScreen extends StatelessWidget {
             ),
             SizedBox(height: 25),
 
-            Container(
-              height: 45,
-              width: 160,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-
-                  backgroundColor: Colors.white.withOpacity(0.8), // Slight transparency
-
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(45), // Rounded corners
-                  ),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.language, color: Colors.green),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                onPressed: () {},
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.language, color: Colors.green), // Globe icon
-                    SizedBox(width: 8.0), // Space between icon and text
-                    Text(
-                      'English',
-                      style: TextStyle(color: Colors.green),
-                    ),
-                    SizedBox(width: 12.0), // Space before dropdown arrow
-                    Icon(Icons.arrow_drop_down, color: Colors.green), // Dropdown arrow
-                  ],
-                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 10),
               ),
-            ),
+              items: languages.map((String lang) {
+                return DropdownMenuItem(
+                  child: Text(lang),
+                  value: lang,
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedLang = value!;
+                });
+              },
+              value: selectedLang,
+              dropdownColor: Colors.white,
+              icon: Icon(Icons.arrow_drop_down, color: Colors.green),
+            )
+
           ],
         ),
       ),
@@ -84,13 +97,15 @@ class WelcomeScreen extends StatelessWidget {
       floatingActionButton: Padding(
         padding: EdgeInsets.only(bottom: 50),
         child: UiHelper.CustomButton(
-        callback: () {
-          Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => LoginScreen()),
-          );
-        },
-        buttonname: "Agree and continue",
-       )),
+          callback: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LoginScreen()),
+            );
+          },
+          buttonname: "Agree and continue",
+        ),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
